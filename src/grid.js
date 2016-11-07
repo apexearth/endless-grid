1ce67ef55198ed8b2ff595808f4ee1823a75886c
cjacobs <apexearth@gmail.com>
2016-11-06 19:11:00 -0800

    Add .getAdjacent()

--- src/grid.js
+++ src/grid.js
@@ -5,23 +5,33 @@ module.exports = Grid;
 function Grid() {
     var grid = {};
     var minX, maxX, minY, maxY;
-    Object.defineProperties(this, {
-        minX: {get: () => minX},
-        minY: {get: () => minY},
-        maxX: {get: () => maxX},
-        maxY: {get: () => maxY},
-    });
+    Object.defineProperty(this, 'minX', {get: () => minX})
+    Object.defineProperty(this, 'minY', {get: () => minY})
+    Object.defineProperty(this, 'maxX', {get: () => maxX})
+    Object.defineProperty(this, 'maxY', {get: () => maxY})
 
     this.create = function (x, y) {
         minX = minX < x ? minX : x;
         minY = minY < y ? minY : y;
         maxX = maxX > x ? maxX : x;
         maxY = maxY > y ? maxY : y;
-        return grid[`${x},${y}`] = new Block(x, y)
+        return grid[`${x},${y}`] = new Block(x, y);
     };
 
     this.get = function (x, y) {
         return grid[`${x},${y}`] || this.create(x, y);
     };
+
+    this.getAdjacent = function (x, y, distance = 1, includeXY = false) {
+        var adjacent = [];
+        for (var yy = y - distance; yy <= y + distance; yy++) {
+            for (var xx = x - distance; xx <= x + distance; xx++) {
+                if (includeXY || x !== xx || y !== yy) {
+                    adjacent.push(this.get(xx, yy));
+                }
+            }
+        }
+        return adjacent;
+    }
 }
 
--- src/grid.spec.js
+++ src/grid.spec.js
@@ -1,16 +1,18 @@
 var expect = require('chai').expect;
 
 var Grid = require('./grid');
+var Block = require('./block');
 
 describe('grid', () => {
     it('new', () => {
-        var grid = new Grid();
+        let grid = new Grid();
         expect(grid.get).to.be.a('function');
+        expect(grid.getAdjacent).to.be.a('function');
     });
     it('.get()', function () {
-        var grid = new Grid();
+        let grid = new Grid();
 
-        let block = grid.get(0,0);
+        let block = grid.get(0, 0);
         expect(block.x).to.equal(0);
         expect(block.y).to.equal(0);
 
@@ -19,7 +21,7 @@ describe('grid', () => {
         expect(grid.maxX).to.equal(0);
         expect(grid.maxY).to.equal(0);
 
-        block = grid.get(100,100);
+        block = grid.get(100, 100);
         expect(block.x).to.equal(100);
         expect(block.y).to.equal(100);
 
@@ -28,4 +30,51 @@ describe('grid', () => {
         expect(grid.maxX).to.equal(100);
         expect(grid.maxY).to.equal(100);
     });
+    it('.getAdjacent()', function () {
+        let grid = new Grid();
+        let i = 0;
+        let adjacent = grid.getAdjacent(1, 1);
+        adjacent.forEach(block => expect(block.constructor).to.equal(Block));
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('0,0');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('1,0');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('2,0');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('0,1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('2,1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('0,2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('1,2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('2,2');
+
+        i = 0;
+        adjacent = grid.getAdjacent(0, 0, 2, true);
+        adjacent.forEach(block => expect(block.constructor).to.equal(Block));
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-2,-2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-1,-2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('0,-2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('1,-2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('2,-2');
+
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-2,-1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-1,-1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('0,-1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('1,-1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('2,-1');
+
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-2,0');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-1,0');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('0,0');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('1,0');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('2,0');
+
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-2,1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-1,1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('0,1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('1,1');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('2,1');
+
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-2,2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('-1,2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('0,2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('1,2');
+        expect(`${adjacent[i].x},${adjacent[i++].y}`).to.equal('2,2');
+    })
 });
